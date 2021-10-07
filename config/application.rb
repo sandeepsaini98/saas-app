edbc2d44173a7021cb34259ff7ce660377baa1c1
sandeepsaini98 <sandeepsaini85113@gmail.com>
2021-10-07 14:45:04 +0530

    updated milia gem file

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SaasApp
+  class Application < Rails::Application
+     
+    # uncomment to ensure a common layout for devise forms
+    #   config.to_prepare do   # Devise
+    #     Devise::SessionsController.layout "sign"
+    #     Devise::RegistrationsController.layout "sign"
+    #     Devise::ConfirmationsController.layout "sign"
+    #     Devise::PasswordsController.layout "sign"
+    #   end   # Devise
+    # Initialize configuration defaults for originally generated Rails version.
+    # config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
